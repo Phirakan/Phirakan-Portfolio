2d91c1c8de3130510b43caa6c3461f77efd67139
Phirakan Khongphaet <peerakran03g@gmail.com>
2025-01-22 00:23:57 +0700

    Edit file to download
    edit path cv

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         </div>
         {/* Download CV Button */}
         <a
-          href="/Phirakan_CV.pdf" // Correct path to the file in the public folder
+          href="/PhirakanCV.pdf" // Correct path to the file in the public folder
           download
           className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition duration-300 font-mono"
         >
