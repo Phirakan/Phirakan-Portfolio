a3a85696cc7b869567601ed9774374f4ceb41881
Phirakan Khongphaet <peerakran03g@gmail.com>
2025-01-22 15:46:26 +0700

    Change the new footer format

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,12 +3,10 @@ import React from 'react';
 const Footer = () => {
   return (
     <>
-      {/* Footer */}
-      <footer className="bg-blue-500 text-white py-4 w-full">
-        <div className="container mx-auto px-4 text-center">
+     <footer className="mt-8 text-sm  text-white " style={{backgroundColor: '#5157C0'}}>
+          Contact me: peerakan03@gmail.com <br />
           © 2025 Phirakan Khongphet. All rights reserved.
-        </div>
-      </footer>
+        </footer>
     </>
   );
 };
